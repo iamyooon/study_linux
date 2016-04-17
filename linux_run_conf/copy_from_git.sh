8d5ff7a72df42764bf25472ee681b7c28f0681ed
seokhoon.yoon <iamyooon@gmail.com>
2016-04-17 12:54:43 +0900

    linux_run_conf : add configs of linux env & devtool
    bashrc:
    	~/.bashrc
    gitconfig:
    	~/.gitconfig
    set_shortcut:
    	~/.set_devtool_shortcut
    	i.e) git
    vimrc:
    	~/.vimrc
    copy_from_{git,local}.sh:
    	copy this configuration files from local or git

---
+++ linux_run_conf/copy_from_git.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp bashrc ~/.bashrc
+cp gitconfig ~/.gitconfig
+cp set_shortcut ~/.set_devtool_shortcut
+cp vimrc  ~/.vimrc
---
+++ linux_run_conf/copy_from_local.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp ~/.bashrc bashrc
+cp ~/.gitconfig gitconfig
+cp ~/.set_devtool_shortcut set_shortcut
+cp ~/.vimrc vimrc
