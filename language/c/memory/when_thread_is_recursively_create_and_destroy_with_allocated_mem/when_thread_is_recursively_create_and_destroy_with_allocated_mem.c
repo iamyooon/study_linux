7cd3774a290e0e264bd86ed70a752855183e24a0
seokhoon.yoon <iamyooon@gmail.com>
2017-11-23 20:42:39 +0900

    language,c : add example to check malloced area of thread is remain or not

---
+++ language/c/memory/when_thread_is_recursively_create_and_destroy_with_allocated_mem/when_thread_is_recursively_create_and_destroy_with_allocated_mem.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+pthread_t threads[5];
+
+void *thread_main(void *);
+
+int main(void)
+{
+	unsigned int i = 0;
+	int rc;
+	void *status;
+
+	while(1){
+		i++;
+		rc = pthread_create(&threads[0], NULL, &thread_main, (void *)0);
+		if (rc != 0) {
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			return -1;
+		}
+
+		rc = pthread_join(threads[0], &status);
+		if (rc != 0) {
+			printf("ERROR; return code from pthread_join() is %d\n", rc);
+			return -1;
+		}
+		printf("Completed join with thread %d status= %p\n", i, status);
+	}
+	return 0;
+}
+
+void *thread_main(void *arg)
+{
+	int *ptr;
+	ptr = malloc(1024);
+	memset(ptr, 5, 1024);
+	sleep(1);
+	return NULL;
+}
