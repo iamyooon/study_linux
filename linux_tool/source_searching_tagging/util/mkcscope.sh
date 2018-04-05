bae4dc1bb26bc1cc09868f2edca7014ab5c1b247
seokhoon.yoon <iamyooon@gmail.com>
2018-04-05 20:52:26 +0900

    linux_tool : add per-stage excution feature and updating cscope db
    - make cscope db for current path
      ./mkcscope.sh {,1}
    - update cscope file list for specific path
      ./mkcscope.sh 2 PATH1 PATH2
    - generate cscope.out using cscope file
      ./mkcscope.sh 3

--- linux_tool/source_searching_tagging/util/mkcscope.sh
+++ linux_tool/source_searching_tagging/util/mkcscope.sh
@@ -1,7 +1,80 @@
-!/bin/sh
+#!/bin/bash
 
-rm -rf cscope.files
+input_file="cscope.files"
+output_file="cscope.out"
 
-find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print > cscope.files
+cleanup()
+{
+	rm -rf $@
+}
 
-cscope -i cscope.files
+show_menu()
+{
+	echo "1. make cscope db(all)"
+	echo "2. add files to cscope.files"
+	echo "3. update cscope db"
+	echo -n "select:"
+}
+
+mk_cscope_files()
+{
+	echo "argment cnt=$# list=[$@]"
+	op_mode="$1"
+	shift 1
+
+	if [ "$op_mode" == "new" ] ; then
+		find $@ \( -name '*.c' \
+			-o -name '*.cpp' \
+			-o -name '*.cc' \
+			-o -name '*.h' \
+			-o -name '*.s' \
+			-o -name '*.S' \) -print > cscope.files
+	elif [ "$op_mode" == "update" ]; then
+		find $@ \( -name '*.c' \
+			-o -name '*.cpp' \
+			-o -name '*.cc' \
+			-o -name '*.h' \
+			-o -name '*.s' \
+			-o -name '*.S' \) -print >> cscope.files
+	else
+		echo "Invalid mode $op_mode"
+	fi
+}
+
+mk_cscope_out()
+{
+	cscope -i $1
+}
+
+read_user_select()
+{
+	request="$1"
+	arg="$2"
+
+	if [ -z "$request" ] || [ "$request" = "1" ] ; then
+		echo "make cscope db(all)"
+		cleanup $input_file
+		if [ -z "$arg" ] ; then
+			mk_cscope_files "."
+		else
+			shift 2
+			mk_cscope_files "new" $@
+		fi
+		mk_cscope_out $input_file
+		exit 0
+	elif [ "$request" == "2" ]; then
+		echo "add files to cscope.files"
+		shift 1
+		mk_cscope_files "update" $@
+		exit 0
+	elif [ "$request" == "3" ]; then
+		echo "3. update cscope db"
+		mk_cscope_out $input_file
+		exit 0
+	else
+		echo "Invalid input $input"
+		exit -1
+	fi
+}
+
+read_user_select $@
