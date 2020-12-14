1a08b61bf056607ade899f8a6cc5f4012db4ed0d
seokhoon.yoon <seokhoon.yoon@lgepartner.com>
2020-12-14 03:47:37 -0800

    language,c : setudi_getuid -> set_get_uid_gid

--- language/c/set_get_uid_gid/main.c
+++ language/c/set_get_uid_gid/main.c
@@ -4,6 +4,8 @@
 int main(void)
 {
 	int current_uid = getuid();
+	int current_gid = getgid();
+
 	printf("My UID is: %d. My GID is: %d\n", current_uid, getgid());
 	system("/usr/bin/id");
 	if (setuid(0))
@@ -11,12 +13,16 @@ int main(void)
 		perror("setuid");
 		return 1;
 	}
+
 	//I am now root!
 	printf("My UID is: %d. My GID is: %d\n", getuid(), getgid());
 	system("/usr/bin/id");
+
 	//Time to drop back to regular user privileges
 	setuid(current_uid);
+	setgid(current_gid);
 	printf("My UID is: %d. My GID is: %d\n", getuid(), getgid());
 	system("/usr/bin/id");
+
 	return 0;
 }
