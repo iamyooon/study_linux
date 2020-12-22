1a4c558b498ddc7ec0895c1cfb4d7205b1572dd1
seokhoon.yoon <seokhoon.yoon@lgepartner.com>
2020-12-22 13:36:45 +0900

    tool,cpuhogger : add CPUQuota to service file and write multi-threaded hogger

--- tool/cpuhogger/cpuhogger.c
+++ tool/cpuhogger/cpuhogger.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <pthread.h>	// pthread_t, pthread_create()
+#include <unistd.h> 	// sleep()
 
-int main(int argc, char** argv)
+void thread_fn(void *data)
 {
 	int i = 0;
 
+	printf("in thread_fn\n");
 	while(1) {
 		i++;
+	}
+}
+
+int main(int argc, char** argv)
+{
+
+	pthread_t tid1, tid2, tid3, tid4;
+	pthread_create(&tid1, NULL, (void *)thread_fn, NULL);
+	pthread_create(&tid2, NULL, (void *)thread_fn, NULL);
+	pthread_create(&tid3, NULL, (void *)thread_fn, NULL);
+	pthread_create(&tid4, NULL, (void *)thread_fn, NULL);
+
+	while(1) {
+		sleep(1);
 	}
 
 	return 0;
