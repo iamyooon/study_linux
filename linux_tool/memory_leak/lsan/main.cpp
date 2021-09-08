da4f3270cc0c96176bdbe2a0d2d04d1127e9cb6f
seokhoon.yoon <seokhoon.yoon@lgepartner.com>
2021-09-08 18:14:57 +0900

    tool,lsan : infinitely alloc loop added

--- linux_tool/memory_leak/lsan/main.cpp
+++ linux_tool/memory_leak/lsan/main.cpp
@@ -1,8 +1,41 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sanitizer/lsan_interface.h>
+
+void kkk() {
+	malloc(5);
+}
+
 void FooBar() {
 	malloc(7);
 }
-int main() {
-	FooBar();
+
+void handler(int signum) {
+	__lsan_do_leak_check();
+}
+
+int main(int argc, char** argv) {
+	int i=5;
+
+	signal(SIGINT, handler);
+
+	if (argc > 1) {
+		while(i--) {
+			FooBar();
+			sleep(1);
+			printf("%d\n", i);
+		}
+
+		i=3;
+		while(i--) {
+			kkk();
+			sleep(1);
+			printf("%d\n", i);
+		}
+	}else {
+		FooBar();
+	}
 	return 0;
 }
