e204e346e3e09416487e0f1c48b7ec7b1fb47598
swkim.kim <swkim.kim@wewakecorp.com>
2022-08-09 21:32:46 +0900

    hello-tasklet-module : init commit

---
+++ kernel_module/hello-tasklet-module/files/hello-tasklet-module.c
@@ -0,0 +1,34 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static void my_tasklet_handler(unsigned long flag);
+DECLARE_TASKLET(my_tasklet, my_tasklet_handler, 0);
+
+static void my_tasklet_handler(unsigned long flag)
+{
+//	tasklet_disable(&my_tasklet);
+	printk("my_tasklet run: do what the tasklet want to do....\n");
+	printk("my_tasklet check: my_tasklet.data %lu \n",my_tasklet.data);
+//	tasklet_enable(&my_tasklet);
+}
+
+static int hello_tasklet_init(void)
+{
+	printk("module init start. \n");
+	printk("Hello tasklet!\n");
+	tasklet_schedule(&my_tasklet);
+	printk("module init end.\n");
+	return 0;
+}
+
+static void hello_tasklet_exit(void)
+{
+	tasklet_kill(&my_tasklet);
+	printk("Goodbye, tasklet!\n");
+}
+
+module_init(hello_tasklet_init);
+module_exit(hello_tasklet_exit);
